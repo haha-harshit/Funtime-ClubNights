bc047b328f2d5812ced98a9d772d6696b428c027
harshit <bkharshitbk@gmail.com>
2023-11-25 15:34:37 +0530

    connect to tailwind

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import {BrowserRouter, Routes, Route} from "react-router-dom"
 const App = () => {
     return(
         <>
-            <h1>Starting Funtime(ClubNights)</h1>
+            <div className="underline bg-black">Starting Funtime</div>
         </>
     )
 }
